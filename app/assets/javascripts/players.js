789ac423ba18645eba35bbf1bee13be0b0976985
jonmackeyparty <jon@jonmackey.party>
2019-06-26 11:41:33 -0400

    add jquery to manifest, sketch Player class, sketch "view leage" event listener

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -1,8 +1,18 @@
+$(document).ready(function() {
+  attachListeners();
+})
+
+function attachListeners(){
+  $("button#view leagues").on("click", function(){
+    makeLeagues();
+  });
+}
+
 class Player {
   constructor(name, jerseyNumber, position) {
   this.name = name;
   this.jerseyNumber = jerseyNumber;
   this.position = position;
   }
-  
+
 }
